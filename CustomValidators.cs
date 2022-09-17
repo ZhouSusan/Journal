c03bfe86ae1141d307d78375986696b91c321149
ZhouSusan <szhou089@gmail.com>
2022-09-17 06:08:13 -0700

    fixed error in CustomVaildators.cs

--- CustomValidators.cs
+++ CustomValidators.cs
@@ -3,21 +3,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Journal 
 {
-    public class FutureDate : validationAttribute 
+    public class FutureDate : ValidationAttribute
     {
-        protected override validationResult IsValid(object value, ValidationContext validationContext)
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
+            // Because our Date field is optional, can't convert null to date.
             if (value == null)
             {
                 return ValidationResult.Success;
             }
-        }
-        DateTime date = (DateTime)value;
 
-        if (date <= DateTime.Now)
-        {
-            return new ValidationResult("must be in the future")
+            DateTime date = (DateTime)value;
+
+            if (date <= DateTime.Now)
+            {
+                return new ValidationResult("must be in the future.");
+            }
+            return ValidationResult.Success;
         }
-        return ValidationResult.Success;
     }
 }
